db348732b8bc364989d3eea773bce02c63d0c2c9
Cucoriedka27 <snehuliacik.tube@gmail.com>
2025-06-05 18:45:58 +0200

    Added a python Interpreter

---
+++ Interpreter/main.py
@@ -0,0 +1,59 @@
+class InStream:
+    def __init__(self):
+        self.buffer = ""
+
+    def next_char(self):
+        if not self.buffer:
+            self.buffer = list(input())
+
+        char = self.buffer.pop(0)
+        has_more = 1 if self.buffer else 0
+        return char, has_more
+
+def rotate(stack, n):
+    return stack
+
+def main():
+    code = "1110+"
+    ip = 0
+    stack = []
+    instream = InStream()
+
+    while True:
+        if ip >= len(code) or ip < 0:
+            break
+
+        match code[ip]:
+            case '1': 
+                stack.append(1)
+            case '+':
+                print(stack)
+            case '0':
+                op = stack.pop()
+                match op:
+                    case 0:
+                        stack.pop()
+                    case 1:
+                        stack.append(stack.pop() + stack.pop())
+                    case 2:
+                        stack.appent(-stack.pop())
+                    case 3: 
+                        value = stack.pop()
+                        stack.append(value)
+                        stack.append(value)
+                    case 4: 
+                        stack.appent(len(stack))
+                    case -1:
+                        stack = rotate(stack, stack.pop())
+                    case -2:
+                        print(chr(stack.pop))
+                    case -3:
+                        char, has_more = instream.next_char()
+                        stack.append(char)
+                        stack.append(has_more)
+                    case -4:
+                        ip += stack.pop() - 1
+        ip += 1
+
+if __name__ == "__main__":
+    main()
